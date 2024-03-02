1d8c14758c3931eaf412b8522fa7fe6f2ea0eb64
kiritokatklian <casillaskhoren1@gmail.com>
2024-03-02 23:26:52 +0100

    [Update] Popup JS - Added startup logic to show different view based on user auth status

--- background.js
+++ background.js
@@ -1,61 +1,61 @@
-import browser from "webextension-polyfill";
+import browser from 'webextension-polyfill';
 
 async function signIn(email, password) {
-  const url = "https://api.kurozora.app/v1/users/signin"
-  const userAgent = navigator.userAgent
+    const url = 'https://api.kurozora.app/v1/users/signin'
+    const userAgent = navigator.userAgent
 
-  // Extracting browser vendor and version information from userAgent
-  const match = userAgent.match(/(Chrome|Firefox|Safari)\/(\S+)/)
-  const browserVendor = match ? match[1] : 'Unknown'
-  const browserVersion = match ? match[2] : 'Unknown'
+    // Extracting browser vendor and version information from userAgent
+    const match = userAgent.match(/(Chrome|Firefox|Safari)\/(\S+)/)
+    const browserVendor = match ? match[1] : 'Unknown'
+    const browserVersion = match ? match[2] : 'Unknown'
 
-  const deviceInfoMatch = userAgent.match(/\(([^)]+)\)/)
-  const deviceInfo = deviceInfoMatch ? deviceInfoMatch[1] : ''
-  const [deviceModel] = deviceInfo.split(';').map(part => part.trim())
+    const deviceInfoMatch = userAgent.match(/\(([^)]+)\)/)
+    const deviceInfo = deviceInfoMatch ? deviceInfoMatch[1] : ''
+    const [deviceModel] = deviceInfo.split(';').map(part => part.trim())
 
-  try {
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        email: email,
-        password: password,
-        platform: "Web",
-        platform_version: browserVersion,
-        device_vendor: "Apple", // For now, since the API has to be updated.
-        device_model: deviceModel || 'Unknown',
-      })
-    })
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                email: email,
+                password: password,
+                platform: 'Web',
+                platform_version: browserVersion,
+                device_vendor: 'Apple', // For now, since the API has to be updated.
+                device_model: deviceModel || 'Unknown',
+            })
+        })
 
-    const record = await res.json()
-    // Handle the response as needed
+        const record = await res.json()
+        // Handle the response as needed
 
-    // Send the response back to the popup
-    browser.runtime.sendMessage({
-      action: "signInResponse",
-      success: true,
-      authToken: record.authenticationToken
-    })
-  } catch (error) {
-    console.error("Error:", error)
+        // Send the response back to the popup
+        browser.runtime.sendMessage({
+            action: 'signInResponse',
+            success: true,
+            authToken: record.authenticationToken
+        })
+    } catch (error) {
+        console.error('Error:', error)
 
-    // Send an error response back to the popup
-    browser.runtime.sendMessage({
-      action: "signInResponse",
-      success: false,
-      error: error.message
-    })
-  }
+        // Send an error response back to the popup
+        browser.runtime.sendMessage({
+            action: 'signInResponse',
+            success: false,
+            error: error.message
+        })
+    }
 }
 
 // Listen for messages from the popup
 browser.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.action === "signIn") {
-        signIn(request.email, request.password)
-      }
+        if (request.action === 'signIn') {
+            signIn(request.email, request.password)
+        }
     }
 )
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,25 +1,56 @@
-import browser from "webextension-polyfill";
+import browser from 'webextension-polyfill'
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadApp()
+})
+
+function loadApp() {
+    browser.storage.local.get(['token'], async function(result){
+        if (result.token === undefined) {
+            await loadSignInContent()
+        } else {
+            await loadSearchContent()
+        }
+    })
+}
 
 async function submitForm() {
     // Get email and password values
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
 
     // Send a message to the background script to initiate the signIn function
-    browser.runtime.sendMessage({ action: "signIn", email: email, password: password });
+    browser.runtime.sendMessage({ action: 'signIn', email: email, password: password })
 }
 
 // Listen for the response from the background script
 browser.runtime.onMessage.addListener(
     function(response, sender, sendResponse) {
-        if (response.action === "signInResponse") {
+        if (response.action === 'signInResponse') {
             if (response.success) {
-                document.getElementById("authToken").textContent = "Auth Token: " + response.authToken;
-            } else {
-                document.getElementById("authToken").textContent = "Error: " + response.error;
+                browser.storage.local.set({
+                    'token': response.authToken
+                }, function () {
+                    console.log('Token Stored')
+                })
+
+                loadApp()
             }
         }
-    }
-);
 
-document.getElementById("loginButton").addEventListener("click", submitForm);
+        if (response.error) {
+            console.error(response.action + ' error: ' + response.error)
+        }
+    }
+)
+
+async function loadSignInContent() {
+    const res =  await fetch('views/signin.html')
+    document.getElementById('app').innerHTML = await res.text()
+    document.getElementById('loginButton').addEventListener('click', submitForm)
+}
+
+async function loadSearchContent() {
+    const res =  await fetch('views/search.html')
+    document.getElementById('app').innerHTML = await res.text()
+}
