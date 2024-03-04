97db06bf35eeccf76bdca46907bc03d2b96c580f
Yuki2no <eragbonikpeya@gmail.com>
2024-03-04 11:50:29 +0100

    [Update] Popup JS - Added search bar after signin - Added basic Anime & Manga buttons - Added search data with poster, name and genre

--- src/js/popup.js
+++ src/js/popup.js
@@ -50,7 +50,76 @@ async function loadSignInContent() {
     document.getElementById('loginButton').addEventListener('click', submitForm)
 }
 
-async function loadSearchContent() {
+
+    
+async function loadSearchContent(){
+
     const res =  await fetch('views/search.html')
     document.getElementById('app').innerHTML = await res.text()
+
+    document.getElementById('search').addEventListener('keydown', async (e)=>{ 
+        if(e.key === 'Enter'){
+            e.preventDefault()
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    
+    
+//creates a blank environment when making a new search
+    document.getElementById('searchData').innerHTML = "";
+    const searchItem = document.getElementById('search').value
+    document.getElementById('search').value = '';
+    const res2 = await fetch(`https://api.kurozora.app/v1/search?scope=kurozora&types%5B%5D=shows&query=${searchItem}&limit=25`, options)
+    const ans = await res2.json()
+
+    for(let i = 0; i < ans.data.shows.data.length; i++){
+        const animeContainer = document.createElement('div')
+        animeContainer.id = 'animeContainer'
+        const anime = ans.data.shows.data[i].href;
+        const animeName = await fetch('https://api.kurozora.app' + anime)
+        const animeResult = await animeName.json()
+        const animeDetails = document.createElement('div')
+        animeDetails.id = 'animeDetails'
+        const animePoster = document.createElement('img')
+        const animeTitle = document.createElement('p')
+        animeTitle.id = 'animeTitle'
+        const animeGenre = document.createElement('p')
+        animeGenre.id = 'animeGenre'
+        animeGenre.textContent = animeResult.data[0].attributes.genres
+        animeTitle.textContent = animeResult.data[0].attributes.title
+        animePoster.id = 'animePoster'
+        animePoster.src = animeResult.data[0].attributes.poster.url
+        animeDetails.appendChild(animeTitle)
+        animeDetails.appendChild(animeGenre)
+        animeContainer.appendChild(animePoster)
+        animeContainer.appendChild(animeDetails)
+        
+        document.getElementById('searchData').appendChild(animeContainer)
+    }
+        }
+
+})
+
 }
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        toggle(this.id)
+    })
+})
+function toggle(id){
+
+   buttons.forEach((button) =>{
+    if(button.id === id){
+        button.classList.add('bg-orange-500')
+    } else{
+        button.classList.remove('bg-orange-500')
+    }
+   }
+)}
+
