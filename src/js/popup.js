9f71d9d48855cedab2e64cc816af5dfc02083875
Yuki2no <eragbonikpeya@gmail.com>
2024-03-06 17:52:44 +0100

    [Update] Popup.JS - Added button search functionality

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,5 @@
 import browser from 'webextension-polyfill'
-
+let selectedType = "";
 document.addEventListener('DOMContentLoaded', function() {
     loadApp()
 })
@@ -51,75 +51,151 @@ async function loadSignInContent() {
 }
 
 
+
+function configureSearchTypes() {
+    const buttons = document.querySelectorAll("button[id^='types[]=']")
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+             //splits the string into an array of 2 string and uses the '=' as a separator 
+            const type = button.id.split('=')[1] //the value of type is the index of 1 of the array
+            toggleSearchType(type) //function to toggle the different buttons
+        })
+    })
+}
+    async function loadSearchContent(){
+        const res =  await fetch('views/search.html')
+        document.getElementById('app').innerHTML = await res.text()
     
-async function loadSearchContent(){
+        configureSearchTypes()
+    
+        document.getElementById('search').addEventListener('keydown', async (e)=>{
+            if(e.key === 'Enter'){
+                e.preventDefault()
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                }
+    
+                //creates a blank environment when making a new search
+                document.getElementById('searchData').innerHTML = "";
+                const searchItem = document.getElementById('search').value
+                document.getElementById('search').value = '';
+                const res2 = await fetch(`https://api.kurozora.app/v1/search?scope=kurozora&types[]=${selectedType}&query=${searchItem}&limit=25`, options)
+                const ans = await res2.json()
 
-    const res =  await fetch('views/search.html')
-    document.getElementById('app').innerHTML = await res.text()
+                //
+                for(let i = 0; i < ans.data[selectedType].data.length; i++){
+                    const container = document.createElement('div')
+                    container.id = 'container'
+                    const anime = ans.data[selectedType].data[i].href;
+                    const name = await fetch('https://api.kurozora.app' + anime)
+                    const result = await name.json()
 
-    document.getElementById('search').addEventListener('keydown', async (e)=>{ 
-        if(e.key === 'Enter'){
-            e.preventDefault()
-    const options = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
+                    //just the basic information that is created for each data
+                    const details = document.createElement('div')
+                    details.id = 'details'
+                    const poster = document.createElement('img')
+                    poster.id = 'poster'
+                    const title = document.createElement('p')
+                    title.id = 'title'
+                    const genre = document.createElement('p')
+                    genre.id = 'genre'
+
+                    //conditional statements for the individual search processes 
+                    //might be removed if a better solution is implemented
+                    if(selectedType == 'characters'){
+                        poster.src = result.data[0].attributes.profile.url
+                        container.appendChild(poster)
+                        title.textContent = result.data[0].attributes.name
+                        details.appendChild(title)
+                        console.log("CHARACTER")
+                    } else if(selectedType == `studios`){
+                        poster.src = result.data[0].attributes.banner.url
+                        container.appendChild(poster)
+                        title.textContent = result.data[0].attributes.name
+                        details.appendChild(title)
+                        console.log("STUDIO")
+                    } else if(selectedType == 'shows' || selectedType == 'games' || selectedType == 'literature'){
+                        poster.src = result.data[0].attributes.poster.url
+                        title.textContent = result.data[0].attributes.title
+                        genre.textContent = result.data[0].attributes.genres
+                        details.appendChild(title)
+                        details.appendChild(genre)
+                        container.appendChild(poster)
+                        console.log('ANIME GAME AND MANGA')
+                    } else if(selectedType == 'users'){
+                        poster.src = result.data[0].attributes.profile.url
+                        details.appendChild(title)
+                        container.appendChild(poster)
+                        title.textContent = result.data[0].attributes.username
+                        details.appendChild(title)
+                        console.log('USER')
+                    } else if(selectedType == 'people'){
+                        title.textContent = result.data[0].attributes.fullName
+                        poster.src = result.data[0].attributes.profile.url
+                        details.appendChild(title)
+                        container.appendChild(poster)
+                    } else if(selectedType == 'songs'){
+                        title.innerHTML = result.data[0].attributes.title
+                    }
+                    container.appendChild(details)
+
+                    //appending all to the main HTML
+                    document.getElementById('searchData').appendChild(container)
+                }
+            }
+        })
     }
-    
-    
-//creates a blank environment when making a new search
-    document.getElementById('searchData').innerHTML = "";
-    const searchItem = document.getElementById('search').value
-    document.getElementById('search').value = '';
-    const res2 = await fetch(`https://api.kurozora.app/v1/search?scope=kurozora&types%5B%5D=shows&query=${searchItem}&limit=25`, options)
-    const ans = await res2.json()
 
-    for(let i = 0; i < ans.data.shows.data.length; i++){
-        const animeContainer = document.createElement('div')
-        animeContainer.id = 'animeContainer'
-        const anime = ans.data.shows.data[i].href;
-        const animeName = await fetch('https://api.kurozora.app' + anime)
-        const animeResult = await animeName.json()
-        const animeDetails = document.createElement('div')
-        animeDetails.id = 'animeDetails'
-        const animePoster = document.createElement('img')
-        const animeTitle = document.createElement('p')
-        animeTitle.id = 'animeTitle'
-        const animeGenre = document.createElement('p')
-        animeGenre.id = 'animeGenre'
-        animeGenre.textContent = animeResult.data[0].attributes.genres
-        animeTitle.textContent = animeResult.data[0].attributes.title
-        animePoster.id = 'animePoster'
-        animePoster.src = animeResult.data[0].attributes.poster.url
-        animeDetails.appendChild(animeTitle)
-        animeDetails.appendChild(animeGenre)
-        animeContainer.appendChild(animePoster)
-        animeContainer.appendChild(animeDetails)
-        
-        document.getElementById('searchData').appendChild(animeContainer)
-    }
+function toggleSearchType(type) {
+    selectedType = type //assigning selectedType to the name of the id
+    const buttons = document.querySelectorAll("button[id^='types[]=']")
+    buttons.forEach(button => {
+            if (button.id === `types[]=${type}`) {
+                button.classList.add('bg-orange-500')
+                button.classList.add('text-white')
+                console.log(button.id)
+
+                
+            } else{
+                button.classList.add('text-orange-500')
+                button.classList.remove('text-white')
+                button.classList.add('border-orange-500')
+                button.classList.remove('bg-orange-500')
+            }
         }
-
-})
-
+    )
 }
 
-const buttons = document.querySelectorAll('button')
+/*
 
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        toggle(this.id)
-    })
-})
-function toggle(id){
+//switch between dark mode and light mode
+function modeSwitch(){
+    let click = 0;
+document.getElementById('mode').addEventListener('click', ()=>{
 
-   buttons.forEach((button) =>{
-    if(button.id === id){
-        button.classList.add('bg-orange-500')
-    } else{
-        button.classList.remove('bg-orange-500')
+    if(document.getElementById('searchPage').style.backgroundColor = 'white'){
+        click = 0
     }
-   }
-)}
+    
+    else if(document.getElementById('searchPage').style.backgroundColor = 'black'){
+        click = 1
+    }
 
+    if(click === 0){
+        document.getElementById('searchPage').style.backgroundColor = 'black'
+        document.getElementById('searchPage').style.color = 'white'
+        console.log("Yay black")
+    }
+
+    else if(click === 1){
+        document.getElementById('searchPage').style.backgroundColor = 'white'
+        document.getElementById('searchPage').style.color = 'black'
+        console.log("Yay white")
+    }
+})
+}
+*/
